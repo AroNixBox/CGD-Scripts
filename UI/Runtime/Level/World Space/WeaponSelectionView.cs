068e291a7f1297c7c62848ef2f4165bde4eda47f
Nixon-Tower <nixonbusinessmail@gmail.com>
2025-11-29 21:32:00 +0100

    feat: show selected weapon in ui even when out of bounds

--- UI/Runtime/Level/World Space/WeaponSelectionView.cs
+++ UI/Runtime/Level/World Space/WeaponSelectionView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -9,7 +10,7 @@ namespace UI.Runtime.Level.World_Space {
         [SerializeField, Required] RectTransform weaponSelectionMenu;
         [SerializeField, Required] ScrollRect weaponSelectionScroll;
         [SerializeField, Required] WeaponSelectionEntry weaponEntryPrefab;
-        [SerializeField, Required] HorizontalLayoutGroup weaponEntriesParent;
+        [SerializeField, Required] RectTransform weaponEntriesParent;
         readonly List<WeaponSelectionEntry> _weaponEntries = new();
         public void SpawnWeaponSelectionEntry(Sprite iconImage) {
             var weaponEntryInstance = Instantiate(weaponEntryPrefab, weaponEntriesParent.transform);
@@ -18,15 +19,56 @@ namespace UI.Runtime.Level.World_Space {
         }
 
         public void EnableWeaponEntryOutline(int index) {
-            // Disable all other outliones
+            var selectedEntry = _weaponEntries[index];
+            
+            // Outline
             _weaponEntries.ForEach(e => e.DeactivateOutline());
-            ScrollToElement(index);
-            _weaponEntries[index].ActivateOutline();
+            selectedEntry.ActivateOutline();
+            
+            // Go To
+            var targetRect = selectedEntry.GetComponent<RectTransform>();
+            if (targetRect == null) throw new NotSupportedException("WeaponSelectionEntry has no Rect?!");
+            ScrollToElement(targetRect);
         }
 
-        void ScrollToElement(int index) {
-            // TODO
+        void ScrollToElement(RectTransform target) {
+            Canvas.ForceUpdateCanvases();
+        
+            var viewport = weaponSelectionScroll.viewport;
+            var viewportRect = viewport.rect;
+            
+            // Borders
+            var targetViewportPos = viewport.InverseTransformPoint(target.position);
+            var targetRect = target.rect;
+            var targetLeftEdge = targetViewportPos.x + targetRect.xMin;
+            var targetRightEdge = targetViewportPos.x + targetRect.xMax;
+        
+            // Viewport-Grenzen
+            var viewportLeftEdge = viewportRect.xMin;
+            var viewportRightEdge = viewportRect.xMax;
+        
+            float offset;
+
+            // Target is:
+            if (targetLeftEdge < viewportLeftEdge) {
+                // Left out of bounds
+                offset = viewportLeftEdge - targetLeftEdge;
+            }
+            else if (targetRightEdge > viewportRightEdge) {
+                // Right out of bounds
+                offset = -(targetRightEdge - viewportRightEdge);
+            }
+            else {
+                // Target is already fully visible
+                return;
+            }
+
+            weaponEntriesParent.localPosition = new Vector2(
+                weaponEntriesParent.localPosition.x + offset,
+                weaponEntriesParent.localPosition.y
+            );
         }
+
 
         public void HideUI() => weaponSelectionMenu.gameObject.SetActive(false);
         public void ShowUI() => weaponSelectionMenu.gameObject.SetActive(true);
