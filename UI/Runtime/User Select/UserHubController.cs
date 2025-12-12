0546c2f389b751e5fdeb2f0f4098b77ab35b01a4
Nixon-Tower <nixonbusinessmail@gmail.com>
2025-12-12 02:24:18 +0100

    feat: loading-screen

--- UI/Runtime/User Select/UserHubController.cs
+++ UI/Runtime/User Select/UserHubController.cs
@@ -1,16 +1,15 @@
 using System;
-using System.IO;
 using Core.Runtime;
 using Core.Runtime.Service;
 using Cysharp.Threading.Tasks;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace UI.Runtime {
     public class UserHubController : MonoBehaviour {
         [SerializeField, Required] UserHubView view;
         public event Action OnAddUser = delegate { };
+        public event Action OnStartGame = delegate { };
 
         void OnEnable() {
             InitializeHubEntries().Forget();
@@ -23,45 +22,16 @@ namespace UI.Runtime {
             view.OnStartGamePressed -= StartGame;
         }
 
-        void StartGame() {
-            const string sceneName = "Playground_Nixon";
-            if (!IsSceneInBuild(sceneName)) {
-                Debug.LogError($"Scene `{sceneName}` not found in Build Settings.");
-                return;
-            }
-            StartGameAsync(sceneName).Forget();
-        }
-
-        async UniTask StartGameAsync(string sceneName) {
-            var op = SceneManager.LoadSceneAsync(sceneName);
-            // Dont activate instantly
-            if (op != null) {
-                op.allowSceneActivation = false;
-                
-                // Activate Loading UI
-
-                // Wait for laod
-                await UniTask.WaitUntil(() => op.progress >= 0.9f);
-
-                // optional: hier Loading UI schließen / Übergang starten
-
-                // Activate Scene
-                op.allowSceneActivation = true;
-                await UniTask.WaitUntil(() => op.isDone);
-            }
-        }
-
-        bool IsSceneInBuild(string sceneName) {
-            for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++) {
-                var path = SceneUtility.GetScenePathByBuildIndex(i);
-                var name = Path.GetFileNameWithoutExtension(path);
-                if (string.Equals(name, sceneName, StringComparison.OrdinalIgnoreCase)) return true;
-            }
-            return false;
-        }
-
         void AddPressedUser() {
             OnAddUser.Invoke();
+            
+            // Deinitialize Self
+            view.DespawnHubEntries();
+            view.Hide();
+        }
+
+        void StartGame() {
+            OnStartGame.Invoke();
             
             // Deinitialize Self
             view.DespawnHubEntries();
--- UI/Runtime/User Select/UserPanelManager.cs
+++ UI/Runtime/User Select/UserPanelManager.cs
@@ -1,29 +1,35 @@
-﻿using System;
-using Sirenix.OdinInspector;
+﻿using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace UI.Runtime {
     public class UserPanelManager : MonoBehaviour {
         [SerializeField, Required] UserCreationController creationController;
         [SerializeField, Required] UserHubController hubController;
+        [SerializeField, Required] LoadingScreenController loadController;
 
         void OnEnable() {
             creationController.OnSubmitUser += ShowHub;
             hubController.OnAddUser += ShowCreation;
+            hubController.OnStartGame += PrepareStartGame;
+        }
+        void OnDisable() {
+            creationController.OnSubmitUser -= ShowHub;
+            hubController.OnAddUser -= ShowCreation;
+            hubController.OnStartGame -= PrepareStartGame;
+        }
+
+        void PrepareStartGame() {
+            loadController.LoadSceneAsync("Playground_Nixon").Forget();
         }
 
         void Start() {
             ShowHub();
             creationController.Hide();
+            loadController.Hide();
         }
 
         void ShowCreation() => creationController.Show();
 
         void ShowHub() => hubController.Show();
-        
-        void OnDisable() {
-            creationController.OnSubmitUser -= ShowHub;
-            hubController.OnAddUser -= ShowCreation;
-        }
     }
 }
