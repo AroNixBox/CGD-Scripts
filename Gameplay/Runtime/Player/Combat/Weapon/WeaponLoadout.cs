7c9fd8f92341486b3aeb1693a67699383fbfa064
TheKingJo <johannesrieck@kabelmail.de>
2026-01-15 11:43:16 +0100

    adjustments for building

--- Gameplay/Runtime/Player/Combat/Weapon/WeaponLoadout.cs
+++ Gameplay/Runtime/Player/Combat/Weapon/WeaponLoadout.cs
@@ -9,21 +9,22 @@ namespace Gameplay.Runtime.Player.Combat {
         public WeaponLoadoutEntry[] WeaponLoadoutEntries => weaponLoadoutEntries;
         [System.Serializable]
         public class WeaponLoadoutEntry {
-            [ValidateInput(nameof(ValidateUniqueWeapon), "Duplicate WeaponsDatas are not allowed</b>")]
+            //[ValidateInput(nameof(ValidateUniqueWeapon), "Duplicate WeaponsDatas are not allowed</b>")]
             [SerializeField, Required] WeaponData weaponData;
             public WeaponData WeaponData => weaponData;
             [SerializeField] int ammunition;
             public int Ammunition => ammunition;
             
-            bool ValidateUniqueWeapon(WeaponData data) {
-                if (data == null) return true;
+            // TODO
+            //bool ValidateUniqueWeapon(WeaponData data) {
+            //    if (data == null) return true;
 
-                var loadout = UnityEditor.Selection.activeObject as WeaponLoadout;
-                if (loadout == null || loadout.weaponLoadoutEntries == null) return true;
+            //    var loadout = UnityEditor.Selection.activeObject as WeaponLoadout;
+            //    if (loadout == null || loadout.weaponLoadoutEntries == null) return true;
 
-                int count = loadout.weaponLoadoutEntries.Count(e => e?.weaponData == data);
-                return count <= 1;
-            }
+            //    int count = loadout.weaponLoadoutEntries.Count(e => e?.weaponData == data);
+            //    return count <= 1;
+            //}
         }
     }
 
