504fef21e0f2a22ec539e6202f8b62b1e9896c33
Jo <johannesrieck@kabelmail.de>
2026-01-26 19:34:39 +0100

    tar + lava mixing

--- Common/Runtime/TerrainHeightWriter.cs
+++ Common/Runtime/TerrainHeightWriter.cs
@@ -2,7 +2,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainHeightWriter : MonoBehaviour {
-    [SerializeField] private List<Transform> positions;
+    [SerializeField] private List<Transform> positionsTar;
+    [SerializeField] private List<Transform> positionsLava;
 
     [SerializeField] private int resolution = 348;
     [SerializeField] private float worldSize = 128f;
@@ -10,43 +11,69 @@ public class TerrainHeightWriter : MonoBehaviour {
     [SerializeField] private Material heightWriteMaterial;
     [SerializeField] private Material terrainMaterial;
 
-    private RenderTexture heightRT;
+    private RenderTexture heightTar;
+    private RenderTexture heightLava;
+    private int seedTar = 0;
+    private int seedLava = 69;
 
     private void Start() {
-        heightRT = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.R16) {
-            enableRandomWrite = false
-        };
-        heightRT.Create();
+        heightTar = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.R16) {enableRandomWrite = false};
+        heightTar.Create();
+        heightLava = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.R16) {enableRandomWrite = false};
+        heightLava.Create();
 
-        terrainMaterial.SetTexture("_HeightTex", heightRT);
+        terrainMaterial.SetTexture("_HeightTexTar", heightTar);
+        terrainMaterial.SetTexture("_HeightTexLava", heightLava);
     }
 
     //remove, once functionalities are final
     private void Update() {
-        RefreshTexture();
+        RefreshTexture("tar");
+        RefreshTexture("lava");
     }
 
-    public void RefreshTexture() {
-        ChangeTargets(null);
+    public void RefreshTexture(string mode) {
+        ChangeTargets(mode, null);
 
-        Graphics.SetRenderTarget(heightRT);
+        if (mode == "tar") {
+            ReRender(ref heightTar, ref positionsTar, seedTar);
+        } else if (mode == "lava") {
+            ReRender(ref heightLava, ref positionsLava, seedLava);
+        } else {
+            Debug.LogError("Mode " + mode + " unknown!");
+        }
+    }
+
+    private void ReRender(ref RenderTexture map, ref List<Transform> positions, int seed) {
+        Graphics.SetRenderTarget(map);
         GL.Clear(false, true, Color.black);
 
         heightWriteMaterial.SetFloat("_WorldSize", worldSize);
+        heightWriteMaterial.SetFloat("_Seed", seed);
 
         foreach (Transform t in positions) {
             heightWriteMaterial.SetVector("_Center", t.position);
-            Graphics.Blit(null, heightRT, heightWriteMaterial);
+            Graphics.Blit(null, map, heightWriteMaterial);
         }
 
         Graphics.SetRenderTarget(null);
     }
 
-    public void ChangeTargets(Transform pos) {
-        if (pos == null) {
-            positions.RemoveAll(t => t == null);
+    public void ChangeTargets(string mode, Transform pos) {
+        if (mode == "tar") {
+            CheckTable(ref positionsTar, pos);
+        } else if (mode == "lava") {
+            CheckTable(ref positionsLava, pos);
         } else {
-            positions.Add(pos);
+            Debug.LogError("Mode " + mode + " unknown!");
+        }
+    }
+
+    private void CheckTable(ref List<Transform> list, Transform pos) {
+        if (pos == null) {
+            list.RemoveAll(t => t == null);
+        } else {
+            list.Add(pos);
         }
     }
 }
