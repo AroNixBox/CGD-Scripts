8a423bea6ebca5c25829f2a2a871064f1ebe0759
Nixon Mac <nixonbusinessmail@gmail.com>
2026-02-04 22:51:52 +0100

    chore(weapon-selection-entry): forgot to remove ammo-label-ref

--- UI/Runtime/Level/WeaponSelectionEntry.cs
+++ UI/Runtime/Level/WeaponSelectionEntry.cs
@@ -8,8 +8,6 @@ namespace UI.Runtime.Level {
     public class WeaponSelectionEntry : MonoBehaviour {
         // TODO On spawn zero out all positions
         [SerializeField, Required] Image iconImage;
-        [SerializeField, Required] TMP_Text ammoAmountLabel;
-        
         public WeaponData Data { get; private set; }
 
         public void Init(WeaponData data) {
@@ -17,7 +15,6 @@ namespace UI.Runtime.Level {
             SetIconImage(data.MenuIcon);
         }
 
-        public void SetAmmo(int amount) => ammoAmountLabel.text = $"AMMO: {amount}";
         void SetIconImage(Sprite sprite) => iconImage.sprite = sprite;
     }
 }
